bc7853b1e9daca04118f08a41cbfe45813a87a22
Petro Malyi <pmalyi@gmail.com>
2024-02-13 19:07:09 +0200

    Check brackets in the codec

--- Week#1/Check-brackets-in-the-codec/check-brackets-in-the-codec.py
+++ Week#1/Check-brackets-in-the-codec/check-brackets-in-the-codec.py
@@ -3,17 +3,16 @@ def checkBrackets(str):
     for i in range(len(str)):
         if str[i] not in {'(', '[', '{', ')', ']', '}'} :
             continue
+        if str[i] in {'(', '[', '{'}:
+            stack.append((str[i], i + 1))
         else:
-            if str[i] in {'(', '[', '{'}:
-                stack.append((str[i], i + 1))
-            else:
-                if len(stack) == 0:
-                    return i + 1  # Коли сама перша дужка закриваюча
-                top = stack.pop()
-                if top[0] == '(' and str[i] != ')' or \
-                        top[0] == '[' and str[i] != ']' or \
-                        top[0] == '{' and str[i] != '}':
-                    return i + 1  # Номер першої закриваючої дужки для якої немає відкриваючої
+            if len(stack) == 0:
+                return i + 1  # Коли сама перша дужка закриваюча
+            top = stack.pop()
+            if top[0] == '(' and str[i] != ')' or \
+                    top[0] == '[' and str[i] != ']' or \
+                    top[0] == '{' and str[i] != '}':
+                return i + 1  # Номер першої закриваючої дужки для якої немає відкриваючої
     if len(stack) == 0:
         return 'Success'
     else:
