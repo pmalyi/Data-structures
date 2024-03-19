385cb04c32d1981c482ee5c01f1584b4f1584888
Petro Malyi <pmalyi@gmail.com>
2024-03-19 13:42:40 +0200

    Compute tree height

--- Week#1/Compute-tree-height/compute-tree-height-recurs.py
+++ Week#1/Compute-tree-height/compute-tree-height-recurs.py
@@ -7,13 +7,10 @@ def Height(r, tree, n):
     return height
 
 
-inf = open('input.txt', 'r')
 n = int(input())
-#n = int(inf.readline())
 tree = tuple(map(int, input().split()))
-# tree = tuple(map(int, inf.readline().split()))
 root = tree.index(-1)
 Height(root, tree, n)
-print(Height(root))
-# print(memo)
-inf.close()
+print(Height(root, tree, n))
+
+
