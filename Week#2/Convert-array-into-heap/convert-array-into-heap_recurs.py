09612261e30bc1f19d172e9fc6fd4a0c0ca087d5
Petro Malyi <pmalyi@gmail.com>
2024-04-05 14:46:16 +0300

    Convert array into heap iter

---
+++ Week#2/Convert-array-into-heap/convert-array-into-heap_recurs.py
@@ -0,0 +1,41 @@
+def left_child_index(i):
+    return 2 * i + 1
+
+
+def right_child_index(i):
+    return 2 * i + 2
+
+
+def sift_down(curr, H, n, swaps):
+    max_index = curr
+    l = left_child_index(curr)
+    if l < n and H[l] < H[max_index]:
+        max_index = l
+    r = right_child_index(curr)
+    if r < n and H[r] < H[max_index]:
+        max_index = r
+    if max_index != curr:
+        H[curr], H[max_index] = H[max_index], H[curr]
+        swaps.append((curr, max_index))
+        sift_down(max_index, H, n, swaps)
+
+
+def build_heap(data, n):
+    swaps = []
+    for i in range((n - 1) // 2, -1, -1):
+        sift_down(i, data, n, swaps)
+    return swaps
+
+
+def main():
+    n = int(input())
+    data = list(map(int, input().split()))
+    assert len(data) == n
+    swaps = build_heap(data, n)
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
+
+
+if __name__ == "__main__":
+    main()
